b05d08fd4638584d9cdb6bedf36730269d108ad4
Shahbaj <shahbaj@cyphertree.com>
2021-05-20 21:54:42 +0530

    Empty commit for heroku

--- src/fobi/contrib/apps/drf_integration/form_handlers/mail/base.py
+++ src/fobi/contrib/apps/drf_integration/form_handlers/mail/base.py
@@ -17,7 +17,7 @@ from ......helpers import extract_file_path
 __title__ = 'fobi.contrib.apps.drf_integration.' \
             'fobi_integration_form_handlers.mail.base'
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
-__copyright__ = '2014-2019 Artur Barseghyan'
+__copyright__ = '2014-2021 Artur Barseghyan'
 __license__ = 'GPL 2.0/LGPL 2.1'
 __all__ = (
     'MailHandlerPlugin',
